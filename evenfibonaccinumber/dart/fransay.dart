ba3bf9f6976ccfa4c032c91eef4ae838c1caeaf3
Francis Osei Annin <francisannin5@gmail.com>
2023-04-29 16:24:25 +0000

    Complete implementation

---
+++ evenfibonaccinumber/dart/fransay.dart
@@ -0,0 +1,33 @@
+// Author: Fransay
+// Date: 29/04/2023
+// Description: Project Euler(Sum of Even Fibonaccinumbers)
+// Complexity: O(n)
+
+
+
+
+void main(){
+  int naturalNumber = 0; // natural number starting at zero
+  var sumEvenFib = 0; // sum of even fibonacci numbers
+  const LIMIT = 4000000; // fibonacci sequence must not exceed LIMIT
+  var firstFibNumber = naturalNumber; // first fib number 
+  var secondFibNumber = firstFibNumber + 1; // second fib number (firstNumber + 1)
+
+  while (firstFibNumber <= LIMIT){
+    var thirdFibNumber = firstFibNumber + secondFibNumber;
+    if (even(thirdFibNumber)){
+      sumEvenFib = sumEvenFib + thirdFibNumber;
+    }
+    firstFibNumber = secondFibNumber;
+    secondFibNumber = thirdFibNumber;
+  }
+  print("SUM OF EVEN FIB NUMBERS: $sumEvenFib"); 
+}
+
+// even function: checks if a number is even or not
+bool even(int number){
+  if (number % 2 == 0){
+    return true;
+  }
+  return false;
+}
